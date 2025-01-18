f2d2cfbfd2dd479fdff6df296f46056ba95839d8
Roman Nikitin <nikromyt@gmail.com>
2025-01-18 21:58:40 +0100

    it was db added

---
+++ DbContext/FilmsSaverDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Model;
+using System;
+
+namespace FilmsSaverDbContext
+{
+    public class FilmsSaverDbContext : DbContext
+    {
+        public FilmsSaverDbContext(DbContextOptions<FilmsSaverDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Film> Films { get; set; }
+    }
+
+}
---
+++ FilmsSaver.Server/Controllers/LoginController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmsSaver.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LoginController(ILogger<WeatherForecastController> _logger) : ControllerBase
+    {
+        public async Task<IActionResult> Login ()
+        {
+            return Ok("Login was success.");
+        }
+
+        public async Task<IActionResult> Registration ()
+        {
+            return Ok("Registartion was success.");
+        }
+    }
+}
