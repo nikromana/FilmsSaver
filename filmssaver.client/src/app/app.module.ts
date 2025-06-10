452906d4f69a7b84321b8d1c676298077ce268a8
Roman Nikitin <nikromyt@gmail.com>
2025-06-10 20:58:25 +0200

    translator was added

--- filmssaver.client/src/app/app.module.ts
+++ filmssaver.client/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
@@ -14,6 +14,12 @@ import { FilmsSavedComponent } from './Pages/films-saved/films-saved.component';
 import { FilmCardComponent } from './Pages/film-card/film-card.component';
 import { FilmPageComponent } from './Pages/film-page/film-page.component';
 import { AboutComponent } from './Pages/about/about.component';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -29,7 +35,16 @@ import { AboutComponent } from './Pages/about/about.component';
   ],
   imports: [
     BrowserModule, HttpClientModule,
-    AppRoutingModule, FormsModule, NgbModule
+    AppRoutingModule, FormsModule, NgbModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'en',
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
